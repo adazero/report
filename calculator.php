e9a5c50e6d9d91ca68885308090ae23cd3482f89
korakot wongsanupat <korakotzaza@hotmail.com>
2015-10-21 05:21:17 -0400

    update calculator

---
+++ calculator.php
@@ -0,0 +1,41 @@
+<html>
+	<title>Calculator</title>
+	<body>
+		<form method = "POST" action = "calculator.php">
+		<p align = "center">
+			<input type = "text" name = "n1">
+
+			<select name = "oper">
+				<option value = "?">Select Operations</option>
+				<option value = "+">+</option>
+				<option value = "-">-</option>
+				<option value = "*">*</option>
+				<option value = "/">/</option>
+			</select>
+
+			<input type = "text" name = "n2">
+		
+			<input type = "submit" name = "Submit" value = "Calculate">
+
+		</p>
+		</form>
+        </body>
+<?php
+{
+if(isset($_POST['n1']) && !empty($_POST['n1']) && !empty($_POST['n2']))
+{
+$n1     = $_POST['n1'];
+$oper   = $_POST['oper'];
+$n2     = $_POST['n2'];
+{
+if($oper == "+")
+echo "$n1 + $n2 = ".($n1+$n2);
+elseif($oper == "-")
+echo "$n1 - $n2 = ".($n1-$n2);
+elseif($oper == "*")
+echo "$n1 * $n2 = ".($n1*$n2);
+elseif($oper == "/")
+echo "$n1 / $n2 = ".($n1/$n2);
+}
+?>
+</html>
